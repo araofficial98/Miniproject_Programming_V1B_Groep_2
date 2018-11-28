7b0dc7bfb0d8f6665952e7aa2cc3a51330474f26
Ara <araofficial98@gmail.com>
2018-11-28 21:27:32 +0100

    G

--- Begin.py
+++ Begin.py
@@ -8,19 +8,21 @@ import png
 import csv
 
 
+# Deze functie toont het loginscherm
 def toon_loginscherm():
     beginscherm.pack_forget()
     beginscherm_beheer.pack_forget()
     loginscherm.pack()
 
 
+# Deze functie toont het beginscherm.
 def toon_beginscherm():
-    detailscherm.forget
     detailscherm.pack_forget()
     loginscherm.pack_forget()
     beginscherm.pack()
 
 
+# Deze functie toont het toevoegscherm.
 def toon_toevoegscherm():
     detailscherm.pack_forget()
     beginscherm_beheer.pack_forget()
@@ -28,6 +30,7 @@ def toon_toevoegscherm():
     toevoegscherm.pack()
 
 
+# Deze functie toont het beginscherm van de beheerder met alle attributen.
 def toon_beginscherm_beheer():
     detailscherm.pack_forget()
     beginscherm_beheer.pack()
@@ -39,55 +42,26 @@ def toon_beginscherm_beheer():
                    bg="#330d05",
                    bd=5)
     lbl111.pack()
-    titel_lbl11 = Label(master=beginscherm_beheer,
-                        font=('Arial', 18, 'italic'),
-                        text="Film 1: " + str(film_details(0)[0]) + " Jaar: " + str(film_details(0)[1]) +
-                             " Genre: " + str(film_details(0)[2]) + " IMDB Rating: " + str(film_details(0)[4]),
-                        bd=5,
-                        fg='#c4ad5a',
-                        bg='#330d05')
 
-    titel_lbl11.pack(anchor='w')
-    titel_lbl21 = Label(master=beginscherm_beheer,
-                        font=('Arial', 18, 'italic'),
-                        text="Film 2: " + str(film_details(1)[0]) + " Jaar: " + str(film_details(1)[1]) +
-                             " Genre: " + str(film_details(1)[2]) + " IMDB Rating: " + str(film_details(1)[4]),
-                        bd=5,
-                        fg='#c4ad5a',
-                        bg='#330d05')
-
-    titel_lbl21.pack(anchor='w')
-    titel_lbl31 = Label(master=beginscherm_beheer,
-                        font=('Arial', 18, 'italic'),
-                        text="Film 3: " + str(film_details(2)[0]) + " Jaar: " + str(film_details(2)[1]) +
-                             " Genre: " + str(film_details(2)[2]) + " IMDB Rating: " + str(film_details(2)[4]),
-                        bd=5,
-                        fg='#c4ad5a',
-                        bg='#330d05'
-                        )
-
-    titel_lbl31.pack(anchor='w')
-
-    titel_lbl41 = Label(master=beginscherm_beheer,
-                        font=('Arial', 18, 'italic'),
-                        text="Film 4: " + str(film_details(3)[0]) + " Jaar: " + str(film_details(3)[1]) +
-                             " Genre: " + str(film_details(3)[2]) + " IMDB Rating: " + str(film_details(3)[4]),
-                        bd=5,
-                        fg='#c4ad5a',
-                        bg='#330d05'
-                        )
-    titel_lbl41.pack(anchor='w')
-
-    titel_lbl51 = Label(master=beginscherm_beheer,
-                        font=('Arial', 18, 'italic'),
-                        text="Film 5: " + str(film_details(4)[0]) + " Jaar: " + str(film_details(4)[1]) +
-                             " Genre: " + str(film_details(4)[2]) + " IMDB Rating: " + str(film_details(4)[4]),
-                        bd=5,
-                        fg='#c4ad5a',
-                        bg='#330d05'
-                        )
-
-    titel_lbl51.pack(anchor='w')
+    # Try except en for-loop voor het controleren van het bestaan van de film(s)
+    # voor het geval er minder dan vijf films zijn en voor het maken van de labels.
+    films = []
+    try:
+        for i in range(5):
+            films.append(film_details(i))
+    except:
+        print("Error, maximaal aantal films bereikt: ", len(films))
+    v = 1
+    for film in films:
+        titel_lbl = Label(master=beginscherm_beheer,
+                          font=('Arial', 18, 'italic'),
+                          text="Film " + str(v) + ": " + str(film[0]) + " Jaar: " + str(film[1]) +
+                               " Genre: " + str(film[2]) + " IMDB Rating: " + str(film[4]),
+                          bd=5,
+                          fg='#c4ad5a',
+                          bg='#330d05')
+        titel_lbl.pack(anchor='w')
+        v += 1
 
     leeg_lbl1 = Label(master=beginscherm_beheer,
                       bg='#330d05')
@@ -109,12 +83,15 @@ def toon_beginscherm_beheer():
     terug_knop.pack()
 
 
+# Deze functie toont het detailscherm van de film. Hier staan alle gegevens over de film in.
 def toon_detailscherm():
+    global film_entry
+    film_entry = film_details(entryy())
     detailscherm.pack_forget()
     beginscherm.pack_forget()
     detailscherm.pack()
     loginscherm.pack_forget()
-    uitleg = str(film_details(entryy())[7])
+    uitleg = str(film_entry[7])
     if "." not in uitleg:
         uitleg_overzicht = uitleg.split(',')
     else:
@@ -122,14 +99,14 @@ def toon_detailscherm():
 
     if entryy() >= 0:
 
-        totaal_tekst = "Titel: " + str(film_details(entryy())[0]) + "\n"
-        totaal_tekst += "Jaar: " + str(film_details(entryy())[1]) + "\n"
-        totaal_tekst += "Genre(s): " + str(film_details(entryy())[2]) + "\n"
-        totaal_tekst += "Duur: " + str(film_details(entryy())[3]) + "\n"
-        totaal_tekst += "IMDB_rating: " + str(film_details(entryy())[4]) + "\n"
-        totaal_tekst += "Land: " + str(film_details(entryy())[5]) + "\n"
-        totaal_tekst += "Regisseur: " + str(film_details(entryy())[6]) + "\n"
-        totaal_tekst += "Zender: " + str(film_details(entryy())[8]) + "\n"
+        totaal_tekst = "Titel: " + str(film_entry[0]) + "\n"
+        totaal_tekst += "Jaar: " + str(film_entry[1]) + "\n"
+        totaal_tekst += "Genre(s): " + str(film_entry[2]) + "\n"
+        totaal_tekst += "Duur: " + str(film_entry[3]) + "\n"
+        totaal_tekst += "IMDB_rating: " + str(film_entry[4]) + "\n"
+        totaal_tekst += "Land: " + str(film_entry[5]) + "\n"
+        totaal_tekst += "Regisseur: " + str(film_entry[6]) + "\n"
+        totaal_tekst += "Zender: " + str(film_entry[8]) + "\n"
 
         totaal_tekst += "Uitleg: \n"
         for regel in uitleg_overzicht:
@@ -161,6 +138,7 @@ def toon_detailscherm():
         toon_beginscherm()
 
 
+# Deze functie is voor het controleren van de login (van de gebruiker) en het openen van het beginscherm.
 def login():
     gebruikersnaam1 = gebruiker_1.get()
     mail1 = email_1.get()
@@ -171,26 +149,30 @@ def login():
         box.showinfo('info', 'Foutieve Login!')
 
 
+# Deze functie is voor het inloggen als beheerder
 def login_beheer():
     box.showinfo('info', 'Welkom Beheerder')
     toon_beginscherm_beheer()
 
 
+# Deze functie maakt de invoer gelijk aan hoe Pycharm de gekozen film ziet.
 def entryy():
     keuze_film_entry = int(entry.get()) - 1
 
     return keuze_film_entry
 
 
+# Deze functie is voor het overschrijven van het CSV-bestand.
 def writer():
     gebruikersnaam = gebruiker_1.get()
     mail = email_1.get()
     with open('database.csv', 'a', newline='') as myfile:
         writer = csv.writer(myfile, delimiter=';')
-        writer.writerow((gebruikersnaam, mail, str(film_details(entryy())[0])))
+        writer.writerow([gebruikersnaam, mail, str(film_entry[0])])
         myfile.close()
 
 
+# Deze functie is voor de controle en meegeven van de gekozen film.
 def ga_verder():
     if entry.get() in "12345":
 
@@ -208,11 +190,13 @@ def ga_verder():
         toon_beginscherm()
 
 
+# Deze functie is voor de betaalknop.
 def betalen():
     toon_bevestiging()
     writer()
 
 
+# Deze functie maakt de QR-Code aan.
 def aanmaken():
     gebruikersnaam2 = gebruiker_1.get()
     global my_qr
@@ -225,6 +209,7 @@ def aanmaken():
     tooncode()
 
 
+# Deze functie toont de gebruikers.
 def toon_gebruikers():
     file = open('database.csv', 'r')
     lines = file.read()
@@ -235,6 +220,7 @@ def toon_gebruikers():
     file.close()
 
 
+# Deze functie zorgt dat de QR code wordt weergeven.
 def tooncode():
     mail2 = email_1.get()
     gebruikersnaam2 = gebruiker_1.get()
@@ -245,19 +231,17 @@ def tooncode():
 
     global foto
     notification_label.config(image=foto, text="Naam: " + gebruikersnaam2 + " Mail: " + mail2)
-    sublabel.config(text=gebruikersnaam2 + ' zijn/haar QR-code voor: ' + str(film_details(entryy())[0]))
-
-# De QR code wordt weergeven.
+    sublabel.config(text=gebruikersnaam2 + ' zijn/haar QR-code voor: ' + str(film_entry[0]))
 
 
+# Dit is het proces om de qr code op te slaan.
 def opslaan():
     keuze_film_entry = int(entry.get()) - 1
     txt = str(film_details(keuze_film_entry)[0]) + ".png"
     my_qr.png(txt, scale=6)
 
-# Dit is het proces om de qr code op te slaan.
 
-
+# Deze functie toont de bevestiging van de film.
 def toon_bevestiging():
     detailscherm.pack_forget()
     bevestigscherm.pack()
@@ -283,85 +267,61 @@ def toon_bevestiging():
     terug_knop3.pack()
 
 
+# Hieronder staan alle benoemde attributen.
 master_root = Tk()
 
-box = messagebox
 
+# Hieronder staan alle labels, knoppen en informatie van het beginscherm.
 beginscherm = Frame(master=master_root, bg='#330d05')
 beginscherm.pack(fill='both', expand=True)
 
 beginscherm_beheer = Frame(master=master_root, bg='#330d05')
 beginscherm_beheer.pack(fill='both', expand=True)
 
+# Try except en for-loop voor het controleren van het bestaan van de film(s)
+# voor het geval er minder dan vijf films zijn en voor het maken van de labels.
+films = []
+try:
+    for i in range(5):
+        films.append(film_details(i))
+except:
+    print("Error, maximaal aantal films bereikt: ", len(films))
+
+g = 1
+for film in films:
+    titel_lbl = Label(master=beginscherm,
+                      font=('Arial', 18, 'italic'),
+                      text="Film " + str(g) + ": " + str(film[0]) + " Jaar: " + str(film[1]) +
+                           " Genre: " + str(film[2]) + " IMDB Rating: " + str(film[4]),
+                      bd=5,
+                      fg='#c4ad5a',
+                      bg='#330d05')
+    titel_lbl.pack(anchor='w')
+    g += 1
+
+box = messagebox
+
 lbl = Label(master=beginscherm,
             font=('Arial', 20),
-            text='Kies uw film naar keuze:',
+            text='Kies uw filmnummer naar keuze:',
             fg='white',
             bg="#330d05",
             bd=5)
 lbl.pack()
-titel_lbl1 = Label(master=beginscherm,
-                   font=('Arial', 18, 'italic'),
-                   text="Film 1: " + str(film_details(0)[0]) + " Jaar: " + str(film_details(0)[1]) +
-                        " Genre: " + str(film_details(0)[2]) + " IMDB Rating: " + str(film_details(0)[4]),
-                   bd=5,
-                   fg='#c4ad5a',
-                   bg='#330d05')
 
-titel_lbl1.pack(anchor='w')
-titel_lbl2 = Label(master=beginscherm,
-                   font=('Arial', 18, 'italic'),
-                   text="Film 2: " + str(film_details(1)[0]) + " Jaar: " + str(film_details(1)[1]) +
-                        " Genre: " + str(film_details(1)[2]) + " IMDB Rating: " + str(film_details(1)[4]),
-                   bd=5,
-                   fg='#c4ad5a',
-                   bg='#330d05')
 
-titel_lbl2.pack(anchor='w')
-titel_lbl3 = Label(master=beginscherm,
-                   font=('Arial', 18, 'italic'),
-                   text="Film 3: " + str(film_details(2)[0]) + " Jaar: " + str(film_details(2)[1]) +
-                        " Genre: " + str(film_details(2)[2]) + " IMDB Rating: " + str(film_details(2)[4]),
-                   bd=5,
-                   fg='#c4ad5a',
-                   bg='#330d05'
-                   )
-
-titel_lbl3.pack(anchor='w')
-
-titel_lbl4 = Label(master=beginscherm,
-                   font=('Arial', 18, 'italic'),
-                   text="Film 4: " + str(film_details(3)[0]) + " Jaar: " + str(film_details(3)[1]) +
-                        " Genre: " + str(film_details(3)[2]) + " IMDB Rating: " + str(film_details(3)[4]),
-                   bd=5,
-                   fg='#c4ad5a',
-                   bg='#330d05'
-                   )
-titel_lbl4.pack(anchor='w')
-
-titel_lbl5 = Label(master=beginscherm,
-                   font=('Arial', 18, 'italic'),
-                   text="Film 5: " + str(film_details(4)[0]) + " Jaar: " + str(film_details(4)[1]) +
-                        " Genre: " + str(film_details(4)[2]) + " IMDB Rating: " + str(film_details(4)[4]),
-                   bd=5,
-                   fg='#c4ad5a',
-                   bg='#330d05'
-                   )
-
-titel_lbl5.pack(anchor='w')
+# Hieronder staan de attributen van het beginscherm.
 entry = Entry(master=beginscherm,
 
               font=('Arial', 14, 'italic'),
 
               fg='black',
               text="\n")
-
 entry.pack()
 
 leeg_lbl = Label(master=beginscherm,
                  bg='#330d05')
 leeg_lbl.pack()
-
 
 volgende_knop = Button(master=beginscherm,
                        text="Ga verder",
@@ -378,6 +338,8 @@ terug_knop1 = Button(master=beginscherm,
                      command=toon_loginscherm)
 terug_knop1.pack()
 
+
+# Hieronder staan alle schermen/frames van het programma.
 detailscherm = Frame(master=master_root, bg='#330d05')
 detailscherm.pack(fill='both', expand=True)
 
@@ -387,10 +349,11 @@ bevestigscherm.pack(fill='both', expand=True)
 toevoegscherm = Frame(master=master_root, bg='#330d05')
 toevoegscherm.pack(fill='both', expand=True)
 
-
 loginscherm = Frame(master=master_root, bg='#330d05')
 loginscherm.pack(fill='both', expand=True)
 
+
+# Hieronder staan alle attributen van het loginscherm.
 Label1 = Label(master=loginscherm, text='Gebruiker:', fg='#c4ad5a', bg='#330d05', font=('Arial', 14))
 Label1.pack(padx=15, pady=5)
 gebruiker_1 = Entry(master=loginscherm, bd=5)
@@ -410,4 +373,6 @@ btn_beheer = Button(master=loginscherm, text='Bent u aanbieder?', command=login_
 btn_beheer.pack(padx=5)
 
 toon_loginscherm()
+
+
 master_root.mainloop()
